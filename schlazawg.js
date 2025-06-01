faf149f82980363a22bcc0b17128c562394a7479
piku ashk <pikuashk@gmail.com>
2025-06-01 02:58:44 +0000

    epic :3

---
+++ schlazawg.js
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+gsap.registerPlugin(SplitText);
+
+gsap.set("h1", { opacity: 1 });
+
+let split = SplitText.create("#heading", { type: "chars" });
+//now animate each character into place from 20px below, fading in:
+gsap.from(split.chars, {
+  y: 20,
+  autoAlpha: 0,
+  stagger: 0.05
+});2
